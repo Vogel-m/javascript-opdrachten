c34f6544e17c905ab1c72d858145044315a91924
Vogel-m <302542475@student.rocmondriaan.nl>
2022-01-16 17:34:42 +0100

    Create main.js
    opdrachten js voor portfolio

---
+++ main.js
@@ -0,0 +1,66 @@
+//2.6 opdracht 1
+//let hour = prompt(`hoe laat is het? (in uren)`);
+//let minute = prompt(`hoe laat is het (in minuten)`);
+
+//if (hour < 6) {
+    //document.body.innerHTML += `het is ${hour}:${minute} 's nachts<br><br>`;
+//} else if (hour >= 6 && hour < 12) {
+    //document.body.innerHTML += `het is ${hour}:${minute} 's morgens<br><br>`;
+//} else if (hour >= 12 && hour < 18) {
+   //document.body.innerHTML += `het is ${hour}:${minute} 's middags<br><br>`;
+//} else if (hour >= 18 && hour < 24) {
+    //document.body.innerHTML += `het is ${hour}:${minute} 's avonds<br><br>`;
+//} else if (hour > 24 && hour < 0) {
+    //alert(`gebruik wel de 24-uurs notatie voor tijd in uren<br><br>`);
+//}
+
+//2.6 opdracht 2
+//let priceExBTW = prompt(`noem een bedrag onder de 40`);
+
+//if (priceExBTW > 10 && priceExBTW <= 20) {
+    //priceIncBTW - (priceExBTW + 1,06).toFixed(2);
+//} else if (priceExBTW > 20 && priceExBTW < 40) {
+    //priceIncBTW - (priceExBTW + 1.21).toFixed(2);
+//} else if (priceExBTW <= 10 && priceExBTW > 0) {
+    //priceIncBTW - priceExBTW.toFixed(2);
+//} else if (priceExBTW > 40) {
+    //alert (`onder de 40 alstublieft`);
+//}else if (priceExBTW <=0) {
+    //alert (`een nul of er onder telt niet`);
+//}
+
+//let btw = (priceIncBTW - priceExBTW).toFixed(2);
+
+//document.body.innerHTML += `uw prijs exclusief BTW  is ${priceExBTW}.<br><br>`;
+//document.body.innerHTML += `uw prijs inclusief btw is ${priceIncBTW}.<br><br>`;
+//document.body.innerHTML += `uw btw is ${btw}.<br><br>`;
+
+// 2.4 opdracht 3
+
+//let num1 = prompt(`noem een getal`);
+//let num2 = prompt(`noem nog een getal`);
+
+//if (num1 > num2) {
+    //document.body.innerHTML += `het grootste getal is ${num1}.<br><br>`;
+//} else if (num2 > num1) {
+    //document.body.innerHTML += `het grootste getal is ${num2}.<br><br>`;
+//}
+//deel van de opdracht was melden of het kleiner zou kunnen, ik ben bang dat ik het niet kleiner kan maken als dit
+
+//2.6 opdracht 4
+//let animal = prompt(`noem een dier`)
+
+//if (animal == "Paard") {
+    //document.body.innerHTML += `het ${animal} hinnikt.`
+//} else if (animal == "Hond") {
+    //document.body.innerHTML += `de ${animal} blaft.`
+//} else if (animal == "Kat") {
+    //document.body.innerHTML += `de ${animal} mieauwt.`
+//} else if (animal == "Vis") {
+    //document.body.innerHTML += `de ${animal} kan geen geluid maken, misschien blub.`
+//} else if (animal == "Kikker") {
+    //document.body.innerHTML += `de ${animal} kwaakt.`
+//} else {
+    //document.body.innerHTML += ` dit dier staat niet in ons systeem`;
+//}
+
